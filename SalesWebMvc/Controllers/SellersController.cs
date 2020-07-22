9ec5dc01781e81e916c9fc04519abdcee9f5f4a5
Rafaela Machado Pires <rafaelamachadopires@gmail.com>
2020-07-22 11:05:38 -0300

    Created page edit seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
+using System.Collections.Generic;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -73,6 +75,44 @@ namespace SalesWebMvc.Controllers
             }
             return View(obj);
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SelerFormViewModel viewModel = new SelerFormViewModel
+            { Seller = obj, Departments = departments};
+            return View(viewModel);
 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
